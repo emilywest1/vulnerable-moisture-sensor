1d6a3d408e48df08df66e9ab376e617aea6f4cb6
emilywest1 <146793504+emilywest1@users.noreply.github.com>
2025-07-12 13:05:49 -0700

    docker stuff

---
+++ Makefile
@@ -0,0 +1,12 @@
+
+
+
+dockerbuild:
+
+
+
+dockerclean:
+
+
+
+dockerbuildrun:
--- main.go
+++ main.go
@@ -8,18 +8,16 @@ import (
 	"bufio"
 )
 
+// Run a terminal command
 func runCommand(name string, args ...string) {
-	// Create a new Cmd struct, representing the external command.
 	cmd := exec.Command(name, args...)
-
-	// Run the command and wait for it to complete.
 	err := cmd.Run()
-
 	if err != nil{
 		log.Fatalf("Command Failed: %v", err)
 	}
 }
 
+// Get current working directory
 func getCWD() string {
 	dir, err := os.Getwd()
 	if err != nil {
@@ -30,16 +28,16 @@ func getCWD() string {
 
 
 func main() {
-	
 
 	runCommand("docker", "rm", "-f", "apache-server")
 
 	runCommand("docker", "run", "-d", "--name", "apache-server", "-p", "8080:80","-v", 
 	fmt.Sprintf("%s/static:/usr/local/apache2/htdocs:ro", getCWD()), "httpd:2.4")
 
+	log.Println("Apache server started at http://localhost:8080		IP Address:")
 
-	log.Println("Apache container started at http://localhost:8080")
-
+	
+b
 	fmt.Println("\nPress 'Enter' to close")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 
